bbe7a05a8de187445bbc612b7cfc478817e3f282
GlacierW <glacier314@gmail.com>
2014-10-20 13:55:43 +0800

    add testing on invalid input date

--- nextdate.cc
+++ nextdate.cc
@@ -4,5 +4,22 @@
 const int DAY_MAX[MONTH_MAX + 1] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 int nextdate( int year, int month, int day ) {
-	return ERR_INVALID_DATE;
+	
+	int n_year  = year,
+		n_month = month,
+		n_day   = day;
+	
+	// validate year, month, and day
+	if( 
+		year  < YEAR_MIN
+	||	month < MONTH_MIN
+	||	day   < DAY_MIN
+	||	year  > YEAR_MAX
+	||	month > MONTH_MAX
+	||	day   > DAY_MAX[month]
+	) {
+		return ERR_INVALID_DATE;
+	}
+	
+	return 0xdeadbeef;
 }
